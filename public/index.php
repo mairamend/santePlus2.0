49d76b535b5406c9243d18a65542bd1b75df5c6d
Mairame Thierno Ndiath <mariemethierno@gmail.com>
2026-04-18 02:02:18 +0000

    Ajout du controller medecin

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@ session_start();
 require_once __DIR__ . '/../app/Database.php';
 require_once __DIR__ . '/../app/Router.php';
 require_once __DIR__ . '/../app/controllers/patientController.php';
+require_once __DIR__ . '/../app/controllers/medecinController.php';
 $router = new Router();
 require_once __DIR__ . '/../routes/web.php';
 $router->run();
